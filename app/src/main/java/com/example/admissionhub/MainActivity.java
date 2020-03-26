25a36bc8471d80e74ee326415f45620eab33c2b9
Kamrozzaman <kamrozzaman63@gmail.com>
2020-03-26 23:54:29 +0600

    loading and login design

---
+++ app/src/main/java/com/example/admissionhub/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.admissionhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4500;
+    AnimationDrawable animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView loading =  (ImageView)findViewById(R.id.loading);
+         animation = (AnimationDrawable)loading.getDrawable();
+         animation.start();
+        new Handler().postDelayed(new Runnable() {
+
+            public void run() {
+
+                Intent one = new Intent(MainActivity.this,logSign.class);
+                startActivity(one);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+        getSupportActionBar().hide();
+
+    }
+}
---
+++ app/src/main/java/com/example/admissionhub/logSign.java
@@ -0,0 +1,16 @@
+package com.example.admissionhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class logSign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_sign);
+
+        getSupportActionBar().hide();
+    }
+}
