4c401cc2f65c07b7943397de34e73287d22a0494
rahat-chy <rht345@gmail.com>
2020-03-27 03:12:20 +0600

    signup and forgot password design

--- app/src/main/java/com/example/admissionhub/logSign.java
+++ app/src/main/java/com/example/admissionhub/logSign.java
@@ -2,15 +2,33 @@ package com.example.admissionhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class logSign extends AppCompatActivity {
+public class logSign extends AppCompatActivity implements View.OnClickListener{
 
+    TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_sign);
 
         getSupportActionBar().hide();
+
+        signup = (TextView) findViewById(R.id.signup);
+        signup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId()==R.id.signup)
+        {
+            Intent intent = new Intent(logSign.this,signUp.class);
+            startActivity(intent);
+        }
+
     }
 }
--- app/src/main/java/com/example/admissionhub/signUp.java
+++ app/src/main/java/com/example/admissionhub/signUp.java
@@ -3,12 +3,37 @@ package com.example.admissionhub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class signUp extends AppCompatActivity {
+public class signUp extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    String[] Group_names;
+    Spinner group;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Group_names = getResources().getStringArray(R.array.Group_names);
+        group = (Spinner) findViewById(R.id.group);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Group_names,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        group.setAdapter(adapter);
+        group.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
